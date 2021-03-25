58644f15ed6cb1220af5605649797739647e6916
mbish08 <mdrew8@gmail.com>
2021-03-25 11:19:35 -0700

    commit tree c92d3183134cea044a9d86dbfedc936985fa63fd

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+function mapToNegativize(array) {
+    let newArray = []
+    for (let i=0; i < array.length; i++) {
+        newArray.push(-1 * array[i])
+    }
+    return newArray
+}
+
+function mapToNoChange(array) {
+    let newArray = []
+    for (let i=0; i < array.length; i++) {
+        newArray.push(array[i])
+    }
+    return newArray
+}
+
+function mapToDouble(array) {
+    let newArray = []
+    for (let i=0; i < array.length; i++) {
+        newArray.push(2 * array[i])
+    }
+    return newArray
+}
+
+function mapToSquare(array) {
+    let newArray = []
+    for (let i=0; i < array.length; i++) {
+        newArray.push(array[i] * array[i])
+    }
+    return newArray
+}
+
+function reduceToTotal(array, starting=0) {
+    let total = starting
+    for (let i=0; i < array.length; i++) {
+        total += array[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(array) {
+    for (let i=0; i < array.length; i++) {
+        if (!array[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(array) {
+    for (let i=0; i < array.length; i++) {
+        if (array[i]) return true
+    }
+    return false
+}
